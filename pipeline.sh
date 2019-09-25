f5fc39404149b7e9628d2557560f5ffded564dfd
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2019-09-25 10:47:09 +0100

    simplify some parts of code

--- pipeline.sh
+++ pipeline.sh
@@ -15,12 +15,17 @@ output_dir=/data/output_files/1_trimmomatic_output
 if [ ! -d "$output_dir" ]; then
 	mkdir -p $output_dir
 
+	# make list of all reads in path, remove everyting after "R" and remove duplicates
 	read_ids=`ls $PATH_TO_READS | sed 's/\(_R\).*/\1/g' | sort -u`
+
+	# for every unqiue read id, run trimmomatic on R1 and R2
 	for read_id in $read_ids
 	do
-		echo $read_id
-		read_1=`paste <(echo "$read_id") <(echo "1.fastq.gz") --delimiters ''`
-		read_2=`paste <(echo "$read_id") <(echo "2.fastq.gz") --delimiters ''`
+		read_1=$read_id  #e.g. read_1=NA12707_R
+		read_1+="1.fastq.gz"  #e.g. read_1=NA12707_R1.fastq.gz
+		read_2=$read_id  #e.g. read_2=NA12707_R
+		read_2+="2.fastq.gz"  #e.g. read_2=NA12707_R2.fastq.gz
+
 		$trimmomatic PE -phred33 -threads $NB_THREADS -trimlog $output_dir/$read_id \
 			$PATH_TO_READS/$read_1 $PATH_TO_READS/$read_2 \
 			$output_dir/$read_1 $output_dir/$read_1.unpaired \
@@ -31,17 +36,24 @@ fi
 
 
 # samtools index ref genome
-if [ ! -f "$PATH_TO_REF_GENOME.fai" ]; then
+## if the file "$PATH_TO_REF_GENOME.fai" doesn't exist, then do samtools faidx
+if [ ! -f "$PATH_TO_REF_GENOME.fai" ]
+then
 	cd $(dirname $PATH_TO_REF_GENOME) && samtools faidx $PATH_TO_REF_GENOME
 fi
 
 # bwa index ref genome
-if [ ! -f "$PATH_TO_REF_GENOME.bwt" ]; then
+## if the file "$PATH_TO_REF_GENOME.bwt" doesn't exist, then do bwa index
+if [ ! -f "$PATH_TO_REF_GENOME.bwt" ]
+then
 	cd $(dirname $PATH_TO_REF_GENOME) && bwa index -a is $PATH_TO_REF_GENOME
 fi
 
 # create seq dictionary
-if [ ! -f "$(dirname $PATH_TO_REF_GENOME)/picard_dict.dict" ]; then
+## if the file "picard_dict.dict", inside the folder containing $PATH_TO_REF_GENOME, doesn't exist,
+## then do  piacard CreateSequenceDictionary
+if [ ! -f "$(dirname $PATH_TO_REF_GENOME)/picard_dict.dict" ]
+then
 	cd $(dirname $PATH_TO_REF_GENOME) && $picard CreateSequenceDictionary R=$PATH_TO_REF_GENOME O=picard_dict.dict
 fi
 
@@ -54,8 +66,10 @@ if [ ! -d "$output_dir" ]; then
 	read_ids=`ls $PATH_TO_READS | sed 's/\(_R\).*/\1/g' | sort -u`
 	for read_id in $read_ids
 	do
-		read_1=`paste <(echo "$read_id") <(echo "1.fastq.gz") --delimiters ''`
-		read_2=`paste <(echo "$read_id") <(echo "2.fastq.gz") --delimiters ''`
+		read_1=$read_id  #e.g. read_1=NA12707_R
+		read_1+="1.fastq.gz"  #e.g. read_1=NA12707_R1.fastq.gz
+		read_2=$read_id  #e.g. read_2=NA12707_R
+		read_2+="2.fastq.gz"  #e.g. read_2=NA12707_R2.fastq.gz
 
 		bwa mem -t $NB_THREADS -R '@RG\tID:'"$n"'\tLB:library\tPL:Illumina\tPU:lane'"$n"'\tSM:yeast' \
 		$PATH_TO_REF_GENOME \
@@ -64,14 +78,12 @@ if [ ! -d "$output_dir" ]; then
 		samtools sort -@ $NB_THREADS $output_dir/$read_id.unsorted.bam $output_dir/$read_id.sorted.bam
 		cd $output_dir && samtools index $read_id.sorted.bam && cd ..
 
-		#$picard MarkDuplicates INPUT=$output_dir/$read_id.sorted.bam OUTPUT=$output_dir/$read_id.deduped.sorted.bam METRICS_FILE=$output/$read_id.dupl_metrics.txt
 	done
 fi
 
 
+
 # Use GATK for variant calling
-
-
 
 
 
